af97cda93a4f4909ec17266e307e170cf637ebf2
Sadegh777 <msm.officially@gmail>
2022-08-02 19:19:38 -0400

    first Update

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+app.use('/posts', postRoutes);
+//db connection
+// const CONNECTION_URL = 'mongodb+srv://msm1390:Sadegh.1390@cluster0.bk0qr.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
